dcd068aa8ba8b2955dbe782739a01fb5f666fa80
the-vegetarian-vampire <lawniczak.john@gmail.com>
2024-10-27 20:27:43 -0400

    Exclude target directory and update 10-17-24

--- LearningRust/target/debug/deps/LearningRust-66fb2dae5e188332.d
+++
@@ -1,5 +0,0 @@
-/Users/johnlawniczak/Desktop/RustBox/LearningRust/target/debug/deps/LearningRust-66fb2dae5e188332: src/main.rs
-
-/Users/johnlawniczak/Desktop/RustBox/LearningRust/target/debug/deps/LearningRust-66fb2dae5e188332.d: src/main.rs
-
-src/main.rs:
