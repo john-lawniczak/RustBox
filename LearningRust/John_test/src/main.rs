d0b3b64fb0479dcb46ae58811dc9c2244ffb0976
the-vegetarian-vampire <lawniczak.john@gmail.com>
2024-11-03 15:39:35 -0500

    Re-added John_test as a regular directory

---
+++ LearningRust/John_test/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+
+    let x: u32 = 5;
+    println!("Hello, world!");
+    println!("{}",x);
+}
---
+++ LearningRust/guessing_game.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // Trim the input and attempt to parse it as an i32
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number.");
+            return;
+        }
+    };
+
+    println!("You guessed: {guess}");
+}
